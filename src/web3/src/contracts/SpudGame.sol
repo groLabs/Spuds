f0644982c79fb3d4c5c783a571239c7536e8c877
SHAKOTN <shakotn@protonmail.com>
2023-11-23 12:53:45 +0200

    feat: basic functionality to start the game

--- src/web3/src/contracts/SpudGame.sol
+++ src/web3/src/contracts/SpudGame.sol
@@ -1,4 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract SpudGame {}
+import {Spud} from "./SpudNFT.sol";
+
+library GameErrors {
+    // Min fee error
+    error MinFeeError();
+}
+
+contract SpudGame {
+    /////////////////////////////////////////////////////////////////////////////
+    //                                  Constants                              //
+    /////////////////////////////////////////////////////////////////////////////
+    // Arbitrary, should be 0.01 ether
+    uint256 public constant INITIAL_FEE = 10000000000000000;
+    uint256 public constant GAME_DURATION = 1 days;
+    Spud public immutable SPUD;
+    /////////////////////////////////////////////////////////////////////////////
+    //                                  Storage                                //
+    /////////////////////////////////////////////////////////////////////////////
+
+    struct Game {
+        address currentOwner;
+        uint256 prizePool;
+        uint256 deadline;
+        uint256 currentTokenId;
+    }
+
+    mapping(uint256 => Game) public games;
+    uint256 public currentGameNonce = 0;
+
+    // Create Spud contract
+    constructor() {
+        SPUD = new Spud(address(this));
+    }
+
+    /// @notice Start the game and pay initial fee. NFT is minted to this contract and can be stolen
+    function startGame() external payable {
+        if (msg.value < INITIAL_FEE) {
+            revert GameErrors.MinFeeError();
+        }
+        // Mint NFT to this contract
+        SPUD.mintNewSpud();
+        // Create game
+        games[currentGameNonce] = Game({
+            currentOwner: msg.sender,
+            prizePool: msg.value,
+            deadline: block.timestamp + GAME_DURATION,
+            currentTokenId: SPUD.currentTokenId() - 1
+        });
+        // Increase game nonce
+        currentGameNonce++;
+    }
+}
--- src/web3/src/contracts/SpudNFT.sol
+++ src/web3/src/contracts/SpudNFT.sol
@@ -4,5 +4,18 @@ pragma solidity 0.8.20;
 import {ERC1155} from "../../../../lib/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 
 contract Spud is ERC1155 {
-    constructor() ERC1155("https://spud.finance/api/token/{id}.json") {}
+    address public immutable GAME_CONTRACT;
+    uint256 public currentTokenId = 0;
+
+    constructor(address gameContract) ERC1155("https://spud.finance/api/token/{id}.json") {
+        GAME_CONTRACT = gameContract;
+    }
+
+    /// @notice External mint function for Spud, can be used only by the game contract
+    function mintNewSpud() external {
+        require(msg.sender == GAME_CONTRACT, "Spud: Only game contract can mint");
+        // Check the current token ID
+        _mint(msg.sender, currentTokenId, 1, "");
+        currentTokenId++;
+    }
 }
